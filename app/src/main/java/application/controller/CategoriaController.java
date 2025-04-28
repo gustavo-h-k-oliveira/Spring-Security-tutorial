f9d499dab354fffa00e6d094abe5359eb051fb62
Gustavo Oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-04-28 17:42:53 +0000

    Adicionando autenticação com rotas seguras e públicas.

---
+++ app/src/main/java/application/controller/CategoriaController.java
@@ -0,0 +1,26 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.record.CategoriaDTO;
+import application.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    @Autowired
+    private CategoriaService categoriaService;
+
+    public Iterable<CategoriaDTO> getAll() {
+        return categoriaService.getAll();
+    }
+
+    @PostMapping
+    public CategoriaDTO insert(@RequestBody CategoriaDTO categoria) {
+        return categoriaService.insert(categoria);
+    }
+}
